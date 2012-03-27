4499dd71d9aac6dba4766d1c6835f035cba16357
Alexander Vetter <vettera@gmail.com>
2012-03-27 10:54:19 +0200

    MultiActionButton is a JButton with a List of action commands and its ui names.

---
+++ src/org/kaffeezusatz/commodity/utils/MultiActionButton.java
@@ -0,0 +1,40 @@
+package org.kaffeezusatz.commodity.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JButton;
+
+public class MultiActionButton extends JButton {
+	private static final long serialVersionUID = 1L;
+	
+	private Map<String, String> actions;
+	private String defaultAction;
+	
+	public MultiActionButton(Object defaultAction, String defaultText) {
+		this.actions = new HashMap<String, String>();
+		this.defaultAction = String.valueOf(defaultAction);
+		
+		addAction(String.valueOf(defaultAction), defaultText);
+		defaultAction();
+	}
+	
+	public MultiActionButton addAction(Object internal, String text) {
+		actions.put(String.valueOf(internal), text);
+		
+		return this;
+	}
+	
+	public MultiActionButton switchAction(Object internal) {
+		setActionCommand(String.valueOf(internal));
+		setText(actions.get(String.valueOf(internal)));
+		
+		return this;
+	}
+	
+	public MultiActionButton defaultAction() {
+		switchAction(defaultAction);
+		
+		return this;
+	}
+}
