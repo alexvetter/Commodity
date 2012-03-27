614d2609fc1035873866762f109f93fa60fa6cda
Alexander Vetter <vettera@gmail.com>
2012-03-27 10:53:00 +0200

    FileChooserActionListener is a helper class which implements ActionListener and handles a JFileChooser. It sets the selected file to a JTextComponent and you are able to get the selected value.

---
+++ src/org/kaffeezusatz/commodity/utils/FileChooserActionListener.java
@@ -0,0 +1,54 @@
+package org.kaffeezusatz.commodity.utils;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.text.JTextComponent;
+
+public class FileChooserActionListener implements ActionListener {
+	
+	private final JFileChooser fc = new JFileChooser();
+	
+	private Component parent;
+	private JTextComponent field;
+	
+	private File file;
+	
+	public FileChooserActionListener(final Component parent, final JTextComponent field) {
+		this.parent = parent;
+		this.field = field;
+	}
+	
+	public void setFileSelectionMode(int mode) {
+		fc.setFileSelectionMode(mode);
+	}
+	
+	public void setDirectory(File directory) {
+		if (directory.exists() && directory.isDirectory()) {
+			fc.setSelectedFile(directory);
+			file = directory;
+			field.setText(directory.getAbsolutePath());
+		}
+	}
+	
+	@Override
+	public void actionPerformed(final ActionEvent e) {
+		//In response to a button click:
+		int returnVal = fc.showOpenDialog(parent);
+		
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+            file = fc.getSelectedFile();
+            field.setText(file.getAbsolutePath());
+        } else {
+        	//
+        	file = null;
+        }
+	}
+	
+	public File getSelectedFile() {
+		return file;
+	}
+}
