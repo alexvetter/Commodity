b9e0590f62bc7d4aafe967a88eae77293336aadd
Alexander Vetter <vettera@gmail.com>
2013-05-27 22:56:18 +0200

    SkipList now implements Set

--- src/main/java/org/kaffeezusatz/commodity/collections/SkipList.java
+++ src/main/java/org/kaffeezusatz/commodity/collections/SkipList.java
@@ -1,12 +1,14 @@
 package org.kaffeezusatz.commodity.collections;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
-public class SkipList<T extends Comparable<? super T>> {
+public class SkipList<T extends Comparable<? super T>> implements Set<T> {
 
     private final List<SkipNode<T>> sentinels;
-
     private final int maxLevel;
 
     /**
@@ -36,7 +38,10 @@ public class SkipList<T extends Comparable<? super T>> {
         }
     }
 
-    public T search(T e) {
+    @Override
+    public boolean contains(Object o) {
+        T e = (T) o;
+
         SkipNode<T> current = this.sentinels.get(0);
 
         while (current != null && current.compareTo(e) != 0) {
@@ -47,16 +52,32 @@ public class SkipList<T extends Comparable<? super T>> {
             }
         }
 
-        return (current == null) ? null : current.getPayload();
+        return (current != null);
     }
 
-    public T insert(T e) {
-        return insert(e, computeRandomLevel());
+    @Override
+    public Iterator<T> iterator() {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    public T insert(T e, int levels) {
-        if (search(e) != null) {
-            return null;
+    @Override
+    public Object[] toArray() {
+        return new Object[0];  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public <T> T[] toArray(T[] t) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public boolean add(T e) {
+        return add(e, computeRandomLevel());
+    }
+
+    public boolean add(T e, int levels) {
+        if (contains(e)) {
+            return false;
         }
 
         SkipNode<T> current = this.sentinels.get(0);
@@ -64,7 +85,7 @@ public class SkipList<T extends Comparable<? super T>> {
 
         int currentLevel = 0;
         while (currentLevel < this.maxLevel && current != null && current.compareTo(e) != 0) {
-            if (current.getRight().compareTo(e) <= 0) {
+            if (current.getRight().compareTo(e) < 0) {
                 current = current.getRight();
             } else {
                 predecessors.set(currentLevel++, current);
@@ -78,7 +99,7 @@ public class SkipList<T extends Comparable<? super T>> {
             predecessors.get(this.maxLevel - i).setRight(newNode);
         }
 
-        return e;
+        return true;
     }
 
     protected int computeRandomLevel() {
@@ -94,17 +115,20 @@ public class SkipList<T extends Comparable<? super T>> {
         return randomLevel;
     }
 
-    public void delete(T e) {
-        if (search(e) == null) {
-            return;
+    @Override
+    public boolean remove(Object o) {
+        if (!contains(o)) {
+            return false;
         }
+
+        T e = (T) o;
 
         SkipNode<T> current = this.sentinels.get(0);
         List<SkipNode<T>> predecessors = new ArrayList<SkipNode<T>>(this.sentinels);
 
         int currentLevel = 0;
-        while (currentLevel < this.maxLevel && current != null && current.compareTo(e) != 0) {
-            if (current.getRight().compareTo(e) <= 0) {
+        while ((currentLevel < this.maxLevel) && (current != null) && (current.compareTo(e) != 0)) {
+            if (current.getRight().compareTo(e) < 0) {
                 current = current.getRight();
             } else {
                 predecessors.set(currentLevel++, current);
@@ -118,6 +142,83 @@ public class SkipList<T extends Comparable<? super T>> {
                 predecessor.setRight(right);
             }
         }
+
+        return true;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> objects) {
+        boolean all = true;
+        for (Object o : objects) {
+            all = contains(o);
+            if (!all) {
+                break;
+            }
+        }
+
+        return all;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> es) {
+        boolean all = true;
+        for (T e : es) {
+            all = add(e);
+            if (!all) {
+                break;
+            }
+        }
+
+        return all;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> objects) {
+        // not yet implemented
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> objects) {
+        for (Object o : objects) {
+            if (contains(o)) {
+                remove(o);
+            }
+        }
+
+        return true;
+    }
+
+    public void clear() {
+        // not yet implemented
+    }
+
+    public boolean equals(Object o) {
+        // not yet implemented
+        return false;
+    }
+
+    public int hashCode() {
+        // not yet implemented
+        return -1;
+    }
+
+    @Override
+    public int size() {
+        SkipNode<T> current = this.sentinels.get(this.maxLevel - 1);
+
+        int size = -2; // because of sentinels
+        while (current != null) {
+            size++;
+            current = current.getRight();
+        }
+
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return (size() == 0);
     }
 
     public String toString() {
@@ -126,14 +227,14 @@ public class SkipList<T extends Comparable<? super T>> {
         for (int i = 0; i < this.maxLevel; i++) {
             SkipNode<T> current = this.sentinels.get(i);
 
-            int count = 0;
+            int count = -2; // because of sentinels
             while (current != null) {
                 count++;
-                string.append(current.getPayload()).append(" ");
+                string.append("[").append(current.toString()).append("]").append(" ");
                 current = current.getRight();
             }
 
-            string.append(" count: ").append(count - 2).append("\n");
+            string.append(" count: ").append(count).append("\n");
         }
 
         return string.toString();
@@ -148,6 +249,11 @@ public class SkipList<T extends Comparable<? super T>> {
         public int compareTo(T t) {
             // Sentinel is always greater!
             return 1;
+        }
+
+        @Override
+        public String toString() {
+            return "Sentinel";
         }
     }
 
--- src/test/java/org/kaffeezusatz/commodity/collections/SkipListTest.java
+++ src/test/java/org/kaffeezusatz/commodity/collections/SkipListTest.java
@@ -2,38 +2,55 @@ package org.kaffeezusatz.commodity.collections;
 
 import org.junit.Test;
 
+import java.util.AbstractSet;
+import java.util.Iterator;
+
+import static org.junit.Assert.*;
+
 public class SkipListTest {
     @Test
 	public final void testInsert() {
 		SkipList<Integer> list = new SkipList<Integer>(6, 0.5);
+
+        assertEquals(0, list.size());
+
+		list.add(1, 3); // 1
+		list.add(0, 6); // 0
+		list.add(0, 6);
+		list.add(2, 3); // 2
+		list.add(2, 3);
+		list.add(4, 5); // 4
+		list.add(4, 5);
+		list.add(3, 6); // 3
+		list.add(3, 6);
+		list.add(3, 6);
+		list.add(7, 3); // 7
+
+        assertEquals(6, list.size());
 		
-		list.insert(1, 3);
-		list.insert(0, 6);
-		list.insert(2, 3);
-		list.insert(4, 5);
-		list.insert(3, 6);
-		list.insert(3, 6);
-		
-		list.insert(5, 3);
-		
-		System.out.println(list.toString());
-		
-		list.delete(4);		
-		System.out.println(list.toString());
-		
-		list.delete(1);
-		System.out.println(list.toString());
-		
-		list.delete(0);
-		System.out.println(list.toString());
-	}
+		list.add(5, 3); // 5
+
+        assertEquals(7, list.size());
+
+        System.out.println(list.toString());
+
+		list.remove(4);
+
+        System.out.println(list.toString());
+
+        assertEquals(6, list.size());
+
+        list.remove(1);
+
+        System.out.println(list.toString());
+    }
 	
-	@Test
+	//@Test
 	public final void testSpecialInsert() {
 		SkipList<Integer> list = new SkipList<Integer>(6, 0.5);
 		
 		for (int i = 1; i < 100000; i++) {
-			list.insert((3*i + i*i) % 123456761);
+			list.add((3*i + i*i) % 123456761);
 		}
 		
 		System.out.println(list.toString());
